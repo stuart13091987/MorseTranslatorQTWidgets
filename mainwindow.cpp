d12b93c60bb53c5637d0b20e559345b03596c412
Lukasz Stanek <lukasz.stanek.ssx@gmail.com>
2017-10-15 20:43:48 +0200

    Added Saving file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,9 @@
 #include "iostream"
 #include "QDebug"
 #include "QMenuBar"
+#include "QFile"
+#include "QFileDialog"
+#include "QMessageBox"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -63,10 +66,44 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::on_OpenButton_clicked()
 {
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Text"), "/home/", tr("Text Files (*.txt )"));
+
+    if (fileName.isEmpty())
+        return;
+    else
+    {
+        QFile file(fileName);
+        if (!file.open(QIODevice::WriteOnly))
+        {
+            QMessageBox::information(this, tr("Unable to open file"),
+                file.errorString());
+            return;
+        }
+
+        QString line;
+        ui->textEdit->clear();
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text))
+        {
+            QTextStream stream(&file);
+            while (!stream.atEnd()){
+                line = stream.readLine();
+                ui->textEdit->setText(ui->textEdit->toPlainText()+line+"\n");
+            }
+        }
+    file.close();
+
+
+    }
+
 
 }
 
 void MainWindow::on_SaveButton_clicked()
 {
-
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Save File"), "/home/",
+                                                    tr("Text (*.txt)"));
+    QFile outputFile(fileName);
+    outputFile.open(QIODevice::WriteOnly | QFile::Text);
+    QTextStream stream(&outputFile);
+    stream << ui->textEdit_2->toPlainText();
 }
